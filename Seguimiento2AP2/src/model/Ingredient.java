a840776710fa6ddc8decfbd52b73df785d848ce6
Santiago0212 <sjbs0212@gmail.com>
2022-02-04 10:52:28 -0500

    avance Seguimiento II

--- Seguimiento2AP2/src/model/Ingredient.java
+++ Seguimiento2AP2/src/model/Ingredient.java
@@ -1,5 +1,7 @@
 package model;
 
+import exception.NegativeNumberException;
+
 public class Ingredient {
 	private String name;
 	private double weight;
@@ -17,8 +19,22 @@ public class Ingredient {
 		return weight;
 	}
 	
-	public void addWeight(double additionalW) {
-		weight = weight + additionalW;
+	public void addWeight(double additionalW) throws NegativeNumberException{
+		if(additionalW<0) {
+			throw new NegativeNumberException();
+		}
+		else {
+			weight = weight + additionalW;
+		}
+	}
+	
+	public void removeWeight(double additionalW) throws NegativeNumberException{
+		if(additionalW<0) {
+			throw new NegativeNumberException();
+		}
+		else {
+			weight = weight - additionalW;
+		}
 	}
 	
 }
